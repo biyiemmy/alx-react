8ee18f8cd5cf0fbf6468aa635434164d2c1e5ac6
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-08 20:21:03 +0100

    refactor the code to few lines

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,15 +1,7 @@
 const { fromJS, Map } = require('immutable');
 
 function getImmutableObject(object) {
-    return fromJS(object);
+  return fromJS(object);
 }
 
-const inputObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-const immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
+module.exports = getImmutableObject;
