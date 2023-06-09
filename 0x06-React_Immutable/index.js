a4aac0f64d1a4010c1c9dce4d8c1dd966c65260f
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-09 20:02:10 +0100

    changes to index.js

--- 0x06-React_Immutable/index.js
+++ 0x06-React_Immutable/index.js
@@ -1,10 +1,11 @@
-import getImmutableObject from './0-fromjs.js';
+const getImmutableObject = require('./0-fromjs');
 
 const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
 };
 
-console.log(getImmutableObject(object));
+const immutableMap = getImmutableObject(object);
+console.log(immutableMap);
