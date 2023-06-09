8325d72d2248629aceca76c70645ce4bd08f53b4
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-09 20:56:26 +0100

    refactoring the code

--- 0x06-React_Immutable/index.js
+++ 0x06-React_Immutable/index.js
@@ -1,10 +1,10 @@
-const getImmutableObject = require('./0-fromjs');
+import getImmutableObject from './0-fromjs.js';
 
 const object = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
 };
 
 const immutableMap = getImmutableObject(object);
