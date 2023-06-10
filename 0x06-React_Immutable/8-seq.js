e6b73a00cc806f850ff0fbed280883e11abf387b
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-10 20:57:30 +0100

    correcting lint errors

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,9 +4,13 @@ export default function printBestStudents(object) {
   const seq = Seq(object);
 
   const filtered = seq.filter((student) => {
-    student.firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
-    student.lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
-    return student.score > 70;
+    const firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
+    const lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
+    return {
+      ...student,
+      firstName,
+      lastName
+    };
   });
 
   const JSObject = filtered.toJS();
