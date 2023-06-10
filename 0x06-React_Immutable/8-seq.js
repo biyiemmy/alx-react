118e2a5777dd2ef93e39379e79d8717b34e450ea
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-06-10 21:18:21 +0100

    refactor the code

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -6,9 +6,12 @@ export default function printBestStudents(object) {
   const filtered = seq.filter((student) => {
     const firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
     const lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
-    student.firstName = firstName;
-    student.lastName = lastName;
-    return student.score > 70;
+    const updatedStudent = {
+      ...student,
+      firstName,
+      lastName,
+    };
+    return updatedStudent.score > 70;
   });
 
   const JSObject = filtered.toJS();
